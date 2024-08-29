cd916b00d0347739f7a4791c816783b02d764f0c
vertskater <vertskater@gmx.at>
2024-08-29 12:15:55 +0200

    add start script to package.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //init view Engine EJS
 app.set("views", path.join(__dirname, "views"));
