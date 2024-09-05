b63e4703806bdf5bdb8eb943ad677b12cd794f79
vertskater <vertskater@gmx.at>
2024-09-05 08:59:34 +0200

    change env Variables

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT || "5432",
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT || "5432",
 });
 
 /*module.exports = new Pool({
